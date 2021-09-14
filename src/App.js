349300180e2a89d53db6a11cacb33f33f895fd0a
Aleksandra Baryshnikova <lexie0428@yandex.ru>
2021-09-14 20:36:59 +0300

    feat: add custom hook and animation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import React, { useState, useRef, useMemo } from 'react';
+import React, { useState } from 'react';
 import './styles/App.css'
-import ClassCounter from './components/ClassCounter';
-import Counter from './components/Counter';
-import PostItem from './components/PostItem.jsx';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
 import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
-import MySelect from './components/UI/select/MySelect';
 import PostFilter from './components/PostFilter';
 import MyModal from './components/UI/MyModal/MyModal';
+import { usePosts } from './hooks/usePosts'
 
 function App() {
   const [posts, setPosts] = useState([
@@ -20,16 +16,7 @@ function App() {
   const [filter, setFilter] = useState({ sort: '', query: '' })
   const [modal, setModal] = useState(false)
 
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts;
-  }, [filter.sort, posts])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-  }, [filter.query, sortedPosts])
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
